d0eb3056f8a69fbf4198e965cf9f28be82c32cbe
fsajadi <s.sfatemeh@gmail.com>
2020-06-08 17:07:39 -0400

    Adding routing for each url in the database

--- express_server.js
+++ express_server.js
@@ -24,6 +24,10 @@ app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase.shortURL };
+  res.render("urls_show", templateVars);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
